f49e5d077d7106da4f950cf856ceb311e5175e6e
lqpham21 <lqpham21@yahoo.com>
2023-05-28 23:59:14 -0500

    New Files

---
+++ index2.js
@@ -0,0 +1,27 @@
+// console.log("hello world");
+
+// consolelog(test);
+// console.log(test2);
+
+const test = "test value";
+var test2 = "value";
+
+helloWorld();
+
+function helloWorld() {
+    console.log("hello world")
+}
+
+const firstName = "Lan";
+const lastName = "Pham";
+
+function helloToYou(firstName, lastName) {
+    return "hello to you, " + firstName + " " + lastName;
+    
+}
+
+console.log(firstName);
+
+const message = helloToYou("Susan", "Pham");
+
+console.log(message);
